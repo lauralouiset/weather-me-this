fa190e5513ac238d8fa591a68d4c95f8959b2ce7
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-11-03 16:12:56 -0400

    added blur detail

--- weather-me-this/scripts/scripts.js
+++ weather-me-this/scripts/scripts.js
@@ -1,29 +1,51 @@
 const functionality = {};
 
+//Toggles display of modals
 functionality.toggleAbout = () => {
 	document.getElementById('about_button').addEventListener('click', () => {
 		about.classList.toggle("hidden");
+		$('.forecast').toggleClass('blur');
+	})
+	document.getElementById('close_button').addEventListener('click', () => {
+		about.classList.toggle("hidden");
+		$('.forecast').toggleClass('blur');
 	})
 }
-
-// functionality.showError = () => {
-// 	$('.error').hide();
-// 	$('.error').slideDown( 500 );
-// }
-
 functionality.openModal = () => {
 	$(".weekly_item").click(function () {
 		$('.weekly_modal').addClass('hidden');
 		$(this).children().removeClass('hidden');
 	});
 }
-
 functionality.closeModal = () => {	
 	$('.weekly_modal').on('click', (e) => {
 		e.stopPropagation();
 		$('.weekly_modal').addClass('hidden');
 	});
 }
+
+// Animates display of Error message
+// functionality.showError = () => {
+// 	$('.error').hide();
+// 	$('.error').slideDown( 500 );
+// }
+
+
+// //onClick of ABOUT THIS PROJECT, display details and adds blur to background elements.
+// $('.aboutThisProject').click(function () {
+// 	$('.projectDetails').removeClass('hide')
+// 	$('section, footer, .contactPersonal, .aboutThisProject, .poweredBy, .headerInfo, .dotted').addClass('blur');
+// });
+
+// //close ABOUT THIS PROJECT, hide details, remove blur from background elements
+// $('.fa-window-close').click(function () {
+// 	$('.projectDetails').addClass('hide')
+// 	$('section, footer, .contactPersonal, .aboutThisProject, .poweredBy, .headerInfo, .dotted').removeClass('blur');
+
+// });
+
+
+
 
 // contains all functionality in init function
 functionality.init = () => {
