caebfc9bf2b6ac8954d5d5b3650819ac72e2daeb
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2018-11-09 11:55:50 -0500

    restructured folders

--- command-line-weather/app.js
+++
@@ -1,8 +0,0 @@
-// Command-line weather app using OpenWeather API and node.js
-
-const weather = require('./weather');
-
-// takes queries as entered, converts spaces to underscores
-const query = process.argv.slice(2).join("+").replace(' ', '+');
-
-weather.requestWeather(query);
